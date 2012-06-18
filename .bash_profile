a66b19de94de494bc3ae47976bd61511b10e8fc7
Duc Dang Trong <dtduc1991@gmail.com>
2012-06-18 15:47:34 +0700

    Initial commit

---
+++ .bash_profile
@@ -0,0 +1,1 @@
+/home/ducdt/.rvm/gems/ruby-1.9.3-p194/bin
---
+++ Project/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = '6a8d7d11ffe4ac6dba1aa596d7f1ef586a1f50dc2dab6609363d6b9d6cf64f7a9cfd55ed805eaed346bd75cb5b4f02b4a851938075647b6d92f4779c0db0a434'
