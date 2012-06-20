26103ccf049146d482edaec16f67a139df3b6b5f
Duc Dang Trong <dtduc1991@gmail.com>
2012-06-20 13:34:10 +0700

    Message

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,9 +1,16 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  root :to => 'static_pages#home'
 
-  get "static_pages/help"
 
-  get "static_pages/about"
+  match '/help', to: 'static_pages#help'
+
+  match '/about', to: 'static_pages#about'
+
+  match '/contact', to: 'static_pages#contact'
+
+  match '/signin', to: 'static_pages#signin'
+
+  match '/signup', to: 'static_pages#signup'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
